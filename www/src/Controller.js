efd3c2065f9f859d5d07782a8d5ff0b4d68b6f41
Worawut <tonmanna@gmail.com>
2014-06-29 05:17:04 +0700

    MyFirst Commit

--- www/src/Controller.js
+++ www/src/Controller.js
@@ -1,20 +1,8 @@
-define(['src/App','src/AppFamous'], function (app,famous) {
+define(['src/App','src/AppFamous','src/Directive'], function (app,famous) {
     app.register.controller('HomeController', function ($scope) {
         $scope.Name = "MyCtrl";
         $scope.init = function () {
             //    // your app here
-            var logo = new famous.ImageSurface({
-                size: [200, 200], content: 'http://upload.wikimedia.org/wikipedia/commons/f/fd/Ghostscript_Tiger.svg',
-                classes: ['double-sided']
-            });
-
-            var initialTime = Date.now();
-            var centerSpinModifier = new famous.Modifier({
-                origin: [0.5, 0.5],
-                transform: function () {
-                    return famous.Transform.rotateY(.002 * (Date.now() - initialTime));
-                }
-            });
             var firstSurface = new famous.Surface({
                 size: [document.innerWidth, 400],
                 content: 'hello world',
@@ -25,7 +13,6 @@ define(['src/App','src/AppFamous'], function (app,famous) {
             });
             firstSurface.setContent('<div style="padding-top: 20px">HELLO WORLD</div>');
 
-            famous.mainContext.add(centerSpinModifier).add(logo)
             famous.mainContext.add(firstSurface);
         }
     });
--- www/src/Directive.js
+++ www/src/Directive.js
@@ -1,1 +1,25 @@
+define(['src/App','src/AppFamous'], function (app,famous) {
+    app.register.directive('logo',function(){
+        return{
+            restrict : 'E',
+            template: '',
+            scope : false,
+            link : function(scope,element,attrib)
+            {
+                var logo = new famous.ImageSurface({
+                    size: [200, 200], content: 'http://upload.wikimedia.org/wikipedia/commons/f/fd/Ghostscript_Tiger.svg',
+                    classes: ['double-sided']
+                });
 
+                var initialTime = Date.now();
+                var centerSpinModifier = new famous.Modifier({
+                    origin: [0.5, 0.5],
+                    transform: function () {
+                        return famous.Transform.rotateY(.002 * (Date.now() - initialTime));
+                    }
+                });
+                famous.mainContext.add(centerSpinModifier).add(logo);
+            }
+        }
+    })
+});
